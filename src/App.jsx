44989bceafdd6e4266590cf6f123a8dce3f145ce
santiagodevrel <santiago@chainsafe.io>
2024-09-09 16:18:56 +0300

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+// import modules
+import { Web3 } from "web3";
+import { ORAPlugin, Models, Chain } from "@ora-io/web3-plugin-ora";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fees, setFees] = useState("0 fees");
+  const [account, setAccount] = useState("no account");
+  const [txHash, setTxHash] = useState("no hash");
+  const [result, setResult] = useState("no hash");
+
+  // initialize provider
+  const web3 = new Web3(window.ethereum);
+
+  // register plugin
+  web3.registerPlugin(new ORAPlugin(Chain.SEPOLIA));
+
+  //use plugin
+  async function sendPrompt() {
+    const PROMPT = "create an image of an apple with a tree";
+
+    //  ORA estimate fees
+    const fees = await web3.ora.estimateFee(Models.STABLE_DIFFUSION);
+    setFees(fees.toString());
+
+    // connect wallet
+    const accounts = await web3.eth.requestAccounts();
+    setAccount(accounts[0]);
+
+    // ORA send the prompt
+    const txReceipt = await web3.ora.calculateAIResult(accounts[0], Models.STABLE_DIFFUSION, PROMPT, fees);
+    setTxHash(txReceipt.transactionHash);
+  }
+
+  async function fetchResult() {
+    // ORA fetch result
+    const PROMPT = "create an image of an apple with a tree";
+
+    const result = await web3.ora.getAIResult(Models.STABLE_DIFFUSION, PROMPT);
+    setResult(result);
+  }
+
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,20 +55,15 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Fees: {fees}</p>
+      <p>Account connected: {account}</p>
+      <p>Tx Hash {txHash}</p>
+      <p>Result AI: {result}</p>
+      <button onClick={sendPrompt}>Send prompt to ORA</button>
+      <button onClick={fetchResult}>Fetch result from ORA</button>
+      <div className="card"></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
